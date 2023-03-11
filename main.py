4ff3e369d7df529d7a95bdba09b9f536af118aec
Younsur <1060592547@qq.com>
2023-03-11 16:17:09 +0800

    conflict solved

--- main.py
+++ main.py
@@ -1,16 +1,3 @@
 import os
-<<<<<<< HEAD
-<<<<<<< HEAD
 os.system('python function_学号.py')
-=======
-<<<<<<< HEAD
-os.system('python function_学号.py')
-=======
-os.system('python function_21373019.py')
->>>>>>> 9334d83c6341c790da1f8bccc3c82f8490daf55f
->>>>>>> a85bb860559ffb3a874c3b58213c3d4618aaae16
-os.system('python function_学号.py')
-=======
-os.system('python function_21373019.py')
->>>>>>> 03cfa442963417ea18aaf30f0c7edc45586b488c
-os.system('python function_学号.py')
+
